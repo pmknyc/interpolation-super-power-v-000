be270869d29a15696504cc405aea67bade648a66
Paula McKinley <pmknyc@optimum.net>
2018-08-02 16:51:27 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,7 +5,6 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(list)
-  puts "The rainbow colors in order are:" 
   puts "R: #{list[0]}  O: #{list[1]}  Y: #{list[2]}  G: #{list[3]}  B: #{list[4]}  I: #{list[5]}  V: #{list[6]}" 
 end
 
